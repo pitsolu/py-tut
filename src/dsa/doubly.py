9be65984a351e0b719dafc22480cddbe114c246c
pitsolu <pitsolu@gmail.com>
2025-11-04 20:26:38 +0300

    doubly + test

--- src/dsa/doubly.py
+++ src/dsa/doubly.py
@@ -1,197 +1,78 @@
 class Node:
-  def __init__(self, data):
-    self.data = data
-    self.next = None
-    self.prev = None
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
 
-# A doubly linked list has each node with data and two links to the next and previous nodes, 
-# allowing traversal in both directions.
+class Doubly:
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-# DELETE the last node from the end of the doubly linked list
-def pop(head:Node):
-    if head is None:
-        print("Doubly linked list is empty")
-        return None
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
 
-    if head.next is None:
-        return None
+    def append(self, value):
+        new_node = Node(value)
+        if self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
 
-    current = head
-    while current.next.next:
-        current = current.next
+    def put(self, new_data, pos):
+        new_node = Node(new_data)
+        if pos == 1:
+            new_node.next = self.head
 
-    del_node = current.next
-    current.next = None
-    del del_node
-    return head
+            if self.head is not None:
+                self.head.prev = new_node
 
-# DELETE the first node from the beginning of the doubly linked list
-def unshift(head:Node):
-    if head is None:
-        print("Doubly linked list is empty")
-        return None
+            self.head = new_node
+            return self.head
 
-    if head.next is None:
-        return None
+        curr = self.head
+        
+        for _ in range(1, pos - 1):
+            if curr is None:
+                print("Position is out of bounds.")
+                return self.head
+            curr = curr.next
 
-    new_head = head.next
-    new_head.prev = None
-    del head
-    return new_head
+        if curr is None:
+            print("Position is out of bounds.")
+            return self.head
 
-# DELETE the node at a given position from the doubly linked list
-def rm(head:Node, position:int):
-    if head is None:
-        print("Doubly linked list is empty")
-        return None
+        new_node.prev = curr
+        new_node.next = curr.next
 
-    if position < 0:
-        print("Invalid position")
-        return head
+        curr.next = new_node
+        if new_node.next is not None:
+            new_node.next.prev = new_node
 
-    if position == 0:
-        if head.next:
-            head.next.prev = None
-        return head.next
+        return self.head
 
-    current = head
-    # count = 0
-    # while current and count < position:
-    #     current = current.next
-    #     count += 1
+    def backward(self):
+        current = self.tail
+        elements = []
+        while current:
+            elements.append(str(current.value))
+            current = current.prev
+        return " <- ".join(elements)
 
-    for _ in range(position):
-        current = current.next
-
-    if current is None:
-        print("Position out of range")
-        return head
-
-    if current.next:
-        current.next.prev = current.prev
-    if current.prev:
-        current.prev.next = current.next
-
-    del current
-    return head
-
-# INSERT a node before a given node in a doubly linked list
-# def put(head:Node, node:Node, new_node:Node):
-def put(node:Node, new_node:Node):
-# def put(node, data):
-    if node is None:
-        print("Error: The given node is None")
-        return
-
-    # current = head
-    # while current:
-    #     if current.data == node.data:
-    #         break
-    #     current = current.next
-
-    # current.prev = new_node
-    # current.next = node
-
-    # return  head
-
-    new_node.prev = node.prev
-    new_node.next = node
-
-    if node.prev:
-        node.prev.next = new_node
-
-    node.prev = new_node
-    return node
-
-# INSERT a new node at the end of the doubly linked list
-def append(head:Node, new_node:Node):
-# def append(head, data):
-#     new_node = Node(data)
-    if head is None:
-        return new_node
-
-    current = head
-    while current.next:
-        current = current.next
-
-    current.next = new_node
-    new_node.prev = current
-    return head
-
-# INSERT a new node at the beginning of the doubly linked list
-def shift(head:Node, new_node:Node):
-# def shift(head, data):
-    # new_node = Node(data)
-    new_node.next = head
-    if head:
-        head.prev = new_node
-    return new_node
-
-# TRAVERSE the doubly linked list and print its elements
-def show(head:Node):
-    current = head
-    ls = []
-    while current:
-      # Print current node's data
-        ls.append(current.data)
-        ls.append(" <-> ")  
-        # Move to the next node
-        current = current.next  
-    ls.append("None")
-    return "".join(ls)
-
-def makeLs(ls:list):
-    nodes = {}
-    head = None
-    for item in ls:
-        head = append(head, Node(item))
-    return head
-
-def nodeLs(head:Node):
-    nodes = {}
-    current = head
-    while current:
-        nodes[current.data] = current
-        current = current.next
-    return nodes
-
-# class Doubly:
-#     def __init__(self):
-#         self.head = None
-
-#     def append(self, data):
-#         new_node = Node(data)
-#         if not self.head:
-#             self.head = new_node
-#             return
-#         last = self.head
-#         while last.next:
-#             last = last.next
-#         last.next = new_node
-#         new_node.prev = last
-
-#     def prepend(self, data):
-#         new_node = Node(data)
-#         if self.head:
-#             self.head.prev = new_node
-#         new_node.next = self.head
-#         self.head = new_node
-
-#     def forward(self):
-#         current = self.head
-#         while current:
-#             print(current.data, end=" <-> ")
-#             current = current.next
-#         print("None")
-
-#     def backward(self):
-#         current = self.head
-#         if not current:
-#             print("None")
-#             return
-#         while current.next:
-#             current = current.next
-#         while current:
-#             print(current.data, end=" <-> ")
-#             current = current.prev
-#         print("None")
+    def __str__(self):
+        current = self.head
+        elements = []
+        while current:
+            elements.append(str(current.value))
+            current = current.next
+        return " <-> ".join(elements)
--- tests/dsa/test_doubly.py
+++ tests/dsa/test_doubly.py
@@ -1,35 +1,24 @@
 import pytest
 from src.dsa.doubly import *
 
-head = Node("a")
-head.next = Node("b")
-head.next.next = Node("c")
-before = head.next.next.next = Node("d")
-head.next.next.next.next = Node("e")
-head.next.next.next.next.next = Node("f")
-
-def test_pop():
-	pop(head)
-	assert show(head) == 'a <-> b <-> c <-> d <-> e <-> None'
-
-def test_unshift():
-	h = unshift(head)
-	assert show(h) == "b <-> c <-> d <-> e <-> None"
-
-@pytest.mark.skip(reason="Not working")
-def test_remove():
-	h = rm(head, 1)
-	print(show(h))
-
-@pytest.mark.skip(reason="Not working")
-def test_insert_before():
-	h = put(before, Node("x"))
-	print(show(head))
-
-def test_append():
-	h = append(head, Node("y"))
-	assert show(h) == "a <-> b <-> c <-> d <-> e <-> y <-> None"
+d = Doubly()
+d.prepend("f")
+d.prepend("e")
+d.prepend("d")
+d.prepend("c")
+d.prepend("b")
+d.prepend("a")
 
 def test_prepend():
-	h = shift(head, Node("y"))
-	assert show(h) == "y <-> a <-> b <-> c <-> d <-> e <-> y <-> None"
+	assert str(d) == 'a <-> b <-> c <-> d <-> e <-> f'
+
+def test_append():
+	d.append("g")
+	assert str(d) == 'a <-> b <-> c <-> d <-> e <-> f <-> g'
+
+def test_put():
+	d.put("x", 2)
+	assert str(d) == 'a <-> x <-> b <-> c <-> d <-> e <-> f <-> g'
+
+def test_backward():
+	assert d.backward() == 'g <- f <- e <- d <- c <- b <- x <- a'
